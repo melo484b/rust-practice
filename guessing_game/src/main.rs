140af6a78c72c6051ba32b9a5937e9a181beae9f
mango <melodev484b@gmail.com>
2022-10-18 21:38:29 -0400

    Practice Rust error handling

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -2,6 +2,25 @@ use std::io;
 use std::cmp::Ordering;
 use rand::Rng;
 
+// Guesses are now only created if they are valid
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new (value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {}.", value);
+        }
+
+        Guess { value }
+    }
+
+    pub fn value(&self) -> i32 {
+        self.value
+    }
+}
+
 fn main() {
     println!("Guess the number!");
 
@@ -10,20 +29,20 @@ fn main() {
     loop {
         println!("Please input your guess.");
 
-        let mut guess = String::new();
+        let mut guess_input = String::new();
 
         io::stdin()
-        .read_line(&mut guess)
+        .read_line(&mut guess_input)
         .expect("Failed to read line");
 
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
+        let guess: i32 = match guess_input.trim().parse() {
+            Ok(num) => Guess::new(num),
             Err(_) => continue,
         };
 
         println!("You guessed: {guess}");
 
-        match guess.cmp(&secret_number) {
+        match guess.value().cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
             Ordering::Equal => {
